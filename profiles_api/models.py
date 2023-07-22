81ed68ec72a244e74075cb1ec1ad4b74973e4f55
lakhwinder <lakhwindersinghx2@gmail.com>
2023-07-22 15:18:08 -0400

    configured api view

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
 
 class UserProfileManager(BaseUserManager):
     """Creating custom user profile manager"""
